a4d2f6c85e57d05d4eeeefd48adf5f7e33010c27
Punyasloka Parida <12557052+bitsnorbytes@users.noreply.github.com>
2023-07-16 13:29:50 +0530

    Site Refresh

---
+++ src/routes/+page.svelte
@@ -0,0 +1,45 @@
+
+<div class="landing-page">
+    <header><span class="brand-name">P Y S L</span></header>
+<ul class="skill-list">
+    <li>digitalArt</li>
+    <li>cinema</li>
+    <li>devOps</li>
+</ul>
+<footer>
+    <span class="footer-signature">PODA DEI</span>
+    </footer>
+</div>
+
+<style>
+   @import url('https://fonts.googleapis.com/css2?family=IBM+Plex+Sans:wght@400;700&family=Noto+Sans:wght@400;600&family=Ubuntu+Condensed&display=swap');
+    .landing-page {
+        display: flex;
+        flex-direction: column;
+        justify-content: space-between;
+        align-items: center;
+        height: 100vh;
+        background-color: #FFCD6E;
+        color:#333131;
+        
+    }
+    .brand-name {
+        font-family: 'Ubuntu Condensed', sans-serif;
+        font-size: 2rem;
+    }
+.skill-list {
+    font-family: 'Noto Sans', sans-serif;
+    list-style-type: none;
+    background-color: #FFE08D;
+    padding: 2em;
+    font-size: 3rem;
+    font-weight: 600;
+    color: #333131;
+}
+.footer-signature {
+    font-family: 'IBM Plex Sans', sans-serif;
+    font-weight: 700;
+    color:#230000;
+    font-size: 1.25rem;
+}
+</style>
